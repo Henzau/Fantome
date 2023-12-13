7a70fffdc82b656b68670d7f5274cfa5d40d5195
Beezdone <ultranumb0220@gmail.com>
2023-12-13 15:31:37 +0100

    fragment result xml 1

--- app/src/main/java/com/example/fantomeapp/MainActivity.java
+++ app/src/main/java/com/example/fantomeapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_main_page);
+        setContentView(R.layout.fragment_res_ana);
     }
 }
