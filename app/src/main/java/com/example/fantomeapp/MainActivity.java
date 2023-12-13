2ea4ab906751871bc599fda2a75fbbd4d3095eae
Henzau <118357386+Henzau@users.noreply.github.com>
2023-12-13 13:28:04 +0100

    Java Activity

---
+++ app/src/main/java/com/example/fantomeapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fantomeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
