01f614cd11db6136480da810eacd1dbed98b384b
Henzau <118357386+Henzau@users.noreply.github.com>
2024-01-31 10:41:01 +0100

    Ajout des boutons de couleur + simplification du code en boolean

--- app/src/main/java/com/example/fantomeapp/ResAna.java
+++ app/src/main/java/com/example/fantomeapp/ResAna.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Objects;
 import java.util.function.ObjIntConsumer;
 import com.example.fantomeapp.databinding.FragmentResAnaBinding;
 
@@ -56,14 +58,30 @@ public class ResAna extends Fragment {
             binding.informationAndrobso.setText(getResources().getString(R.string.Hardening_upto14));
         }
 
-        boolean devmod_enable = analyse.isDevMode(this.getContext().getContentResolver());
-        binding.button1.setText("dev mode is : "+ devmod_enable+"\n");
-        binding.button2.setText("adb mode is : " + analyse.isADB(this.getContext().getContentResolver())+"\n"); //necessite des tests !
+        boolean value_button1 = analyse.isDevMode(this.getContext().getContentResolver());
+        boolean value_button2 = analyse.isADB(this.getContext().getContentResolver());
+        String res_button3 = analyse.isPhoneEncrypted(); // a changer en boolean
+        boolean value_button3 = Objects.equals(res_button3, "encrypted");
+        String res_button4 = analyse.isAVB(); // a changer en boolean
+        boolean value_button4 = Objects.equals(res_button4,"enforcing");
+        String res_button5 = analyse.isolation_final(analyse.isolation1(),analyse.isolation2(),analyse.isolation3());
+        String targetString = "Isolation conforme";
+        boolean value_button5 = res_button5.startsWith(targetString);
+        String[] Keysecu = analyse.isInsideSecureHardware();
+        String res_button6 = Keysecu[0];
+        boolean value_button6 = Objects.equals(res_button6,"StrongBox présente");
+        String res_button7 = Keysecu[1];
+        boolean value_button7 = Objects.equals(res_button7,"Sécurité hardware");
+        String value_button8 = analyse.LastSecurityPatch();
 
-        binding.button3.setText("phone encrypted ? : "+analyse.isPhoneEncrypted()+"\n");
-        binding.button4.setText("AVB state : "+analyse.isAVB()+"\n");
+        binding.button1.setText("dev mode is : "+ value_button1+"\n");
 
-        binding.button5.setText(analyse.isolation_final(analyse.isolation1(),analyse.isolation2(),analyse.isolation3()));
+        binding.button2.setText("adb mode is : " + value_button2+"\n"); //necessite des tests !
+
+        binding.button3.setText("phone encrypted ? : "+value_button3+"\n");
+        binding.button4.setText("AVB state : "+res_button4+"\n");
+
+        binding.button5.setText(res_button5);
         binding.imageButtonRes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,14 +93,34 @@ public class ResAna extends Fragment {
             }
         });
 
-        String[] Keysecu = analyse.isInsideSecureHardware();
-        binding.button6.setText(Keysecu[0]);
-        binding.button7.setText(Keysecu[1]);
-        binding.button8.setText("Last Security Patch : " + analyse.LastSecurityPatch()+"\n");
+        binding.button6.setText(res_button6);
+        binding.button7.setText(res_button7);
+        binding.button8.setText("Last Security Patch : " + value_button8+"\n");
 
 
+        //Affichage des boutons de la bonne couleur.
 
-
+        if(value_button1){
+            binding.button1.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.not_good));
+        }
+        if(value_button2){
+            binding.button2.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.not_good));
+        }
+        if(!value_button3){
+            binding.button3.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.not_good));
+        }
+        if(!value_button4){
+            binding.button4.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.not_good));
+        }
+        if(!value_button5){
+            binding.button5.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.not_good));
+        }
+        if(!value_button6){
+            binding.button6.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.not_good));
+        }
+        if(!value_button7){
+            binding.button6.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.not_good));
+        }
 
 
 
@@ -93,7 +131,7 @@ public class ResAna extends Fragment {
                 // The user just clicked
                 if (getActivity() instanceof MainActivity) {
                     MainPage mainpage = MainPage.newInstance();
-                    ((MainActivity) getActivity()).showDialog(binding.button1,0);
+                    ((MainActivity) getActivity()).showDialog(binding.button1, 0);
                 }
             }
         });
